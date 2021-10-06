4a01e6b7d196b40542ddcda232ebd0d3a457093e
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 10:37:03 +0900

    ReactでCSSを弄る

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert("click");
+  const myStyle = {
+    color: "green",
+    fontSize: "18px",
+    marginBottom: "0px"
+  };
   /* returnできる要素は親に一つだけ */
   /* 不要なタグを避けるために<React.Fragment>か<>を使うことで、無用な表記を避けれる */
   return (
     <>
-      <h1>Hello</h1>
-      <p>React write.</p>
+      <h1 style={{ color: "red" }}>Hello</h1>
+      <p style={myStyle}>React write.</p>
       <p>first commit from sandbox</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
