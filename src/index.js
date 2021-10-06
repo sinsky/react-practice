e17c43a6ff54a5ef8656d44f44a41cdfe0884dfb
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 10:22:48 +0900

    add comment JSX表記の基礎的なルールをコメント

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
+  /* returnできる要素は親に一つだけ */
+  /* 不要なタグを避けるために<React.Fragment>か<>を使うことで、無用な表記を避けれる */
   return (
     <>
       <h1>Hello</h1>
