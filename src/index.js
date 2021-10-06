768cc20d81ff7b9740ea0333f8b06a75e218cd60
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 10:28:51 +0900

    Compornent for App Appをコンポーネント化

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  /* returnできる要素は親に一つだけ */
-  /* 不要なタグを避けるために<React.Fragment>か<>を使うことで、無用な表記を避けれる */
-  return (
-    <>
-      <h1>Hello</h1>
-      <p>React write.</p>
-      <p>first commit from sandbox</p>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.querySelector("#root"));
