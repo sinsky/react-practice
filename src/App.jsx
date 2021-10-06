5e2f8a1b4e290db59c7203eed734aa6b300d45cd
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 11:24:32 +0900

    fizzbuzz

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import ColorfullMessage from "./compornents/ColorfullMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, faceShowFunc] = useState(false);
   const onClickCountup = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
-
-  const [faceShowFlag, faceShowFunc] = useState(true);
+  /* 下のif-elseにより使えなくなっている */
   const swichFaceShow = () => {
     faceShowFunc(!faceShowFlag);
   };
+  if (num && num % 3 === 0) faceShowFlag || faceShowFunc(true);
+  else faceShowFlag && faceShowFunc(false);
   /* returnできる要素は親に一つだけ */
   /* 不要なタグを避けるために<React.Fragment>か<>を使うことで、無用な表記を避けれる */
   return (
