93300e164c73779870193bbe73fe84823101f1d5
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 10:35:03 +0900

    Reactでボタンクリックのイベントを使う

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert("click");
   /* returnできる要素は親に一つだけ */
   /* 不要なタグを避けるために<React.Fragment>か<>を使うことで、無用な表記を避けれる */
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <h1>Hello</h1>
       <p>React write.</p>
       <p>first commit from sandbox</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
